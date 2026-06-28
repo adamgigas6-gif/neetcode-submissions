7f1a1236d395f4214f5a1c4f5a1e0abc8941dc7e
adamgigas6-gif <adamgigas6@gmail.com>
2026-06-28 19:04:26 +1000

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> groups;
+
+        for (const auto&s : strs){
+            string sorted = s;
+            sort(sorted.begin(), sorted.end());
+            groups[sorted].push_back(s);
+
+        }
+        vector<vector<string>> result;
+        for (auto&pair : groups){
+            result.push_back(pair.second);
+        }
+    return result;
+    }
+};
